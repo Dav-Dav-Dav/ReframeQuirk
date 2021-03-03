7cf249939d443d43dc4ed042fec7094b6a33b4c9
Dav <davdavdav4@gmail.com>
2021-03-03 20:57:27 +0000

    Local data mapped onto page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import "./styles.css";
 import data from "./data";
 
-console.log(data)
-
+// 1. Mapping over data
 export default function App() {
+  const reframeData = data.map((x, i) => <Hello key={i} reframeNumber={i} />);
+
+  // 2. Organising mapped over data onto the page
+  function Hello(props) {
+    return (
+      <div>
+        <p>{props.reframeNumber}</p>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Reframe</h1>
+      {reframeData}
     </div>
   );
 }
