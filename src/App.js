ffa186a0f18f4d628d8a077aed32fd46a405912f
Dav <davdavdav4@gmail.com>
2021-03-03 21:49:07 +0000

    Basic routing setup

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import "./styles.css";
-import data from "./data";
-import { Link } from "react-router-dom";
+import Homepage from "./Homepage";
+import { Switch, Route } from "react-router-dom";
 
 // 1. Mapping over data
 export default function App() {
-  const reframeData = data.map((x, i) => <Hello key={i} reframeNumber={i} />);
-
-  // 2. Organising mapped over data onto the page
-  function Hello(props) {
-    return (
-      <div>
-        <Link to="/home">{props.reframeNumber}</Link>
-        <br />
-        <br />
-        <br />
-      </div>
-    );
-  }
-
   return (
     <div className="App">
-      <h1>Reframe</h1>
-      <ul>{reframeData}</ul>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/Homepage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import data from "./data";
+import { Link } from "react-router-dom";
+
+export default function Homepage() {
+  const reframeData = data.map((x, i) => <Hello key={i} reframeNumber={i} />);
+
+  // 2. Organising mapped over data onto the page
+  function Hello(props) {
+    return (
+      <div>
+        <Link to="/home">{props.reframeNumber}</Link>
+        <br />
+        <br />
+        <br />
+      </div>
+    );
+  }
+
+  return (
+    <div className="App">
+      <h1>Reframe</h1>
+      <ul>{reframeData}</ul>
+    </div>
+  );
+}
